52d43488960afc012f9ed5814129958e6c0bc6a3
LuanCarlosCar <69122423+LuanCarlosCar@users.noreply.github.com>
2021-04-08 22:00:46 -0300

    js

--- script.js
+++ script.js
@@ -13,19 +13,48 @@ const Modal= {
 
 
 const transactions = [
-    {id:1, description:'luz',amount: -50000,date:'23/01/2021',},
-    {id:2, description:'Criação de Website',amount: 500000,date:'23/01/2021',},
-    {id:3, description:'internet',amount: -20000,date:'23/01/2021',},
+    {id:1, description:'luz',amount:-50000,date:'23/01/2021',},
+    {id:2, description:'Criação de Website',amount:500000,date:'23/01/2021',},
+    {id:3, description:'internet',amount:-20000,date:'23/01/2021',},
 ]
 const transaction = {
+        all:transactions,
+        add(transactions){
+            transaction.all.push(transactions)
+        },
+
+
     incomes(){
+
+        let income =0;
+        transaction.all.forEach(transactions => {
+            if(transactions.amount > 0){
+                income = income + transactions.amount;
+            }
+        })
+
             //somar as entradas
+            return income;
     },
+
     expenses(){
+
+
+        let expens = 0;
+        transactions.all.forEach(transactions =>{
+            if(transactions.amount < 0){
+                expens= expens + transactions.amount;
+            }
+        })
          //somar as saidas
+         return expens;
     },
+
     total(){
+
+  
         //entradas -saidas
+        return transaction.incomes() + transaction.expenses();
     }
 
 }
@@ -63,8 +92,17 @@ const DOM={
 },
 
 
-    updateBalance(){
-        document.getElementById('incomeDislpay').innerHTML= "Discover"
+    updateBalance(){ 
+        document.getElementById('incomeDislpay').innerHTML= Utils.formatCurrency(transaction.incomes())
+
+        document.getElementById('expenseDisplay').innerHTML=  Utils.formatCurrency(transaction.expenses())
+        document.getElementById('totalDisplay').innerHTML=  Utils.formatCurrency(transaction.total())
+        
+
+
+
+
+
     }
 }
 
@@ -72,7 +110,7 @@ const Utils ={
     formatCurrency(value){
         const signal = Number(value) < 0 ? "-" : ""
 
-    alue = String(value).replace(/\d/g,"")
+    alue = String(value).replace(/\D/g,"")
     value= Number(value) / 100
     value=      value.toLocaleString("pt-BR",{
         style: "currency",
@@ -92,3 +130,8 @@ transactions.forEach(function(transaction){
 })
 
 DOM.updateBalance()
+
+
+transaction.add({
+    id: 39,descriptio:'Alo', amount: 200, date: '23/01/21'
+})
